2d09be655f01f6a778b14f1727dc1f4910555a19
Eren Çifci <eren.cifci04@gmail.com>
2022-01-19 15:08:10 +0300

    create client w apollo

---
+++ client/src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+
+import { BrowserRouter } from "react-router-dom";
+
+import { ApolloProvider } from "@apollo/client";
+import client from "./apollo";
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ApolloProvider>,
+  document.getElementById("root")
+);
